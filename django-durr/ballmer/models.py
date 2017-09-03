34ee9681d02104faa6759c4c085aae301081d373
Blake Thompson <bthompson@forumone.com>
2017-09-02 17:08:18 -0700

    Starting state config

--- django-durr/ballmer/models.py
+++ django-durr/ballmer/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class State(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
